8675e76f9ceac6a3b0ad8559f6e34ef06b4e75f4
Leo is awesome <lc599@drexel.edu>
2014-08-28 20:05:56 -0400

    removed bin

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Pi_Joules/NetBeansProjects/Expression_Calculator/bin/AndroidManifest.xml : \
-/Users/Pi_Joules/NetBeansProjects/Expression_Calculator/AndroidManifest.xml \
--- bin/Expression_Calculator-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-/Users/Pi_Joules/NetBeansProjects/Expression_Calculator/bin/Expression_Calculator-debug-unaligned.apk : \
-/Users/Pi_Joules/NetBeansProjects/Expression_Calculator/bin/Expression_Calculator.ap_ \
-/Users/Pi_Joules/NetBeansProjects/Expression_Calculator/bin/classes.dex \
